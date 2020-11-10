f8fd1bc4fd57f3c66a0b6765fbec309a4f8e969c
Nikolozc <nikolozc@buffalo.edu>
2020-11-10 13:56:59 -0500

    progress made

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -17,33 +17,34 @@ class CLI
 
     def welcome
         logo
-        sleep(5)
+        sleep(1)
         system('clear')
         display_menu = @@prompt.select("Welcome! Please Log In or Signup.", %w(Login Signup))
         case display_menu
         when "Login"
-            self.login
+            @@user = User.second
+            self.main_menu
         when "Signup"
             self.signup
         end
     end
 
-    def login
-        username = @@prompt.ask("Please enter a username")
-        password = @@prompt.mask("Please enter password (0-9)")
-        if user = User.find_by(username: username, password: password)
-            @@user = user
-            system('clear')
-            puts "Welcome back #{username}!"
-            sleep(1)
-            self.main_menu
-        else
-            system('clear')
-            puts "User not found, taking you back to the welcome screen"
-            sleep(1)
-            self.welcome
-        end
-    end
+    #def login
+     #   username = @@prompt.ask("Please enter a username")
+      #  password = @@prompt.mask("Please enter password (0-9)")
+       # if user = User.find_by(username: username, password: password)
+        #    @@user = user
+         #   system('clear')
+          #  puts "Welcome back #{username}!"
+           # sleep(1)
+            #self.main_menu
+        #else
+         #   system('clear')
+          #  puts "User not found, taking you back to the welcome screen"
+           # sleep(1)
+            #self.welcome
+        #end
+    #end
 
     def signup
         username = @@prompt.ask("Please enter a username")
@@ -76,15 +77,13 @@ class CLI
         puts "Enter the movie you'd like to search for"
         movie_to_find = gets.chomp
         movies = Movie.all.select{|movie|movie.title.include?(movie_to_find)}
-        binding.pry
         if !movies.empty?
-            selection = @@prompt.select("Please choose which movie you'd like to rate", (movies.map{|movie|movie.title}))
+            selection = @@prompt.select("Please choose which movie you'd like to rate", (movies.map{|movie|movie.title}), per_page: 10)
             puts "What is the rating for this movie? (Ratings are scaled 1-5)"
-            rating = gets.chomp
-            rating = rating.to_i
+            rating = gets.chomp.to_i
             while(rating < 1 || rating > 5) do
                 puts "Invalid choice, please enter a number 1-5"
-                rating = gets.chomp
+                rating = gets.chomp.to_i
             end
             selection = Movie.find_by_title(selection)
             Rating.create({user: @@user, movie: selection, rating: rating})
@@ -96,34 +95,35 @@ class CLI
             puts "No movies found by that title, try again"
             self.search_for_movie
         end
-
-        #movies = @@client.find_by_title(movie_to_find)
-        #movie_title = movies.title
-        #new_movie =Movie.create(title: movie_title)
-        #selection = @@prompt.select("Please choose which movie you'd like to rate", (movie_title))
-        #puts "What is the rating for this movie? (Ratings are scaled 1-5)"
-        #rating = gets.chomp
-        #Rating.create({user: @@user, movie: new_movie, rating: rating})
-        #puts "Your movie has been added to your list of Rated Movies"
-        #sleep (0.5)
-        #puts "Taking you back to the main menu.."
-        #main_menu
     end
 
     def rated_movies
         puts "Here are the movies you have rated already"
         sleep(1)
-        selection = @@prompt.select("Please choose which movies rating you'd like to update", (@@user.movies))
+        rated_movies = @@user.movies.map do |movie| 
+            "#{movie.title} - rated #{movie.ratings.find_by(user: @@user, movie: movie).rating}"
+        end
+        selection = @@prompt.select("Please choose which movies rating you'd like to update", (rated_movies))
+        selection = selection.split(" - " , 2)
         puts "What is the new rating for this movie?"
-        rating = gets.chomp
+        rating = gets.chomp.to_i
+        while(rating < 1 || rating > 5) do
+            puts "Invalid choice, please enter a number 1-5"
+            rating = gets.chomp.to_i
+        end
+        movie = Movie.all.find_by_title(selection[0])
+        Rating.all.find_by(user: @@user,movie: movie).update(rating: rating)
         puts "The rating for this movie has been updated!"
-        sleep (0.5)
+        sleep (2)
         puts "Taking you back to the main menu.."
+        sleep(2)
         main_menu
     end
 
     def random_movie
-        Faker::Movie.title
+        all_movies = Movie.all.map {|movie| movie.title}
+        all_movies
+        sleep(5)
     end
 
     def logout
